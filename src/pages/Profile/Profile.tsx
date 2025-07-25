4d80d8cfe54b6d3f98cb9e63e291d05475a833a6
janm2001 <jan.mackovic@hivetech.hr>
2025-07-25 16:05:53 +0200

    Simple profile

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,5 +1,19 @@
+import { useUser } from "@/context/UserContext";
+import { Heading, VStack } from "@chakra-ui/react";
+
 const Profile = () => {
-  return <div>Profile works!</div>;
+  const { user } = useUser();
+  return (
+    <VStack gap={4} p={4}>
+      <Heading as="h1" size="xl" mb={4}>
+        Profile Page
+      </Heading>
+      {/* Profile details go here */}
+      <p>This is where user profile information will be displayed.</p>
+      {/* Additional profile components can be added here */}
+      <p>Type of user - {user}</p>
+    </VStack>
+  );
 };
 
 export default Profile;
